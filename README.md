e8a485d09bb084ceb054d56aa84fa2712dfdc852
Wallace <w.brunoge@gmail.com>
2021-05-03 15:37:35 -0300

    wb first commit

---
+++ README.md
@@ -0,0 +1,106 @@
+
+# wb_dotfiles
+
+  
+
+## How to install
+1. Execute ````git clone https://github.com/chwiee/wb_dotfiles```` to clone this repo
+2. Use ````bash wb_dotfiles.sh install```` to install and configurate
+
+## Folder tree
+
+  In user home the wb-dotfiles.sh create:
+
+| Folder name | Short Desc |
+|-------------- |------------- |
+| cloud | Cloud Providers Scripts and Documentations |
+| iac | InfraAsCode repo |
+| jenkins | Jenkins configurations and scripts |
+| kubernetes | Examples to Cluster, dayle scripts and helm repos |
+| monitoring | Monitoring repo with scripts and configs to use with elk, zbx, prometheus |
+| scripts | Bash, Shell scripts repo |
+| hascorp | hashcorp applications configs and doc with how to use |
+| python | Python repo |
+| docker | Container docs and scripts |
+| wallpapers | Wallpapers path |
+
+## Bynds
+My primary modkey is <kbd>win</kbd> and secondary is <kbd>Alt</kbd> follow list with ByndKeys and description:
+
+| Keys               |  Desc  |
+|----------------|---------|
+<kbd>win</kbd>+<kbd>w</kbd> | Open firefox browser
+<kbd>win</kbd>+<kbd>shift</kbd>+<kbd>W</kbd> | Open chromium browser 
+<kbd>win</kbd>+<kbd>d</kbd> | Launch rofi
+<kbd>win</kbd>+<kbd>m</kbd> | Hide and Show i3bar
+<kbd>win</kbd>+<kbd>r</kbd> | Enter in Resize mode
+<kbd>win</kbd>+<kbd>return</kbd> | Open terminal
+  
+
+## Custom Commands
+Open terminal and execute follow commands:
+| Commands 		| Action |
+| ------------- | -----  |
+| aliasrc       | To open ~/.conf/aliasrc in vim editor |
+| zshrc         | To open ~/.conf/zshrc in vim editor   |
+| i3rc          | To open ~/.conf/i3 in vim editor      |
+| cd..          | 'cd ..' but not have space :D         |
+| cdd           | To use ranger                         |
+| ll            | ls -ltrh                              |
+| la            | ls -ltrah                             |
+| bkp           | make a bkp all files in your home     |
+
+## Application
+This script install follow applications:
+| Applications |
+| --------- |
+| i3 |
+| tree |
+| git |
+| zsh |
+| software-properties-common |
+| apt-transport-https |
+| arandr |
+| firefox |
+| unzip |
+| curl |
+| wget |
+| feh |
+| compton |
+| cmatrix |
+| python3-pip |
+| python3 |
+| clang |
+| gcc |
+| make |
+
+## Fonts
+Font-Awesome
+fire-code
+
+## Workspace
+In my config I have 9 ws in total each of them has a font-awesome icon linked to them (for better organization) among them we have:
+
+| Workspace | Icon     | Why?                           | 
+| ------    | ---      |  --                            |
+| ws1       | Terminal | All terminals open here        |
+| ws2       | Firefox  | All FireFox Instance open here |
+| ws3       | Cloud    | When i need work with cloud    |
+| ws4       | --       | |
+| ws5       | --       | |
+| ws6       | --       | |   
+| ws7       | --       | |
+| ws8       | Cannabis | Forfun moments (youtube, music)|
+| ws9       | Angry    | Chat apps (teams, slack, ...)  |
+
+
+## Plugins
+### ZSH plugins
+
+| name | link |
+|---|---|
+
+
+## Tks
+https://fontawesome.com/v4.7.0/cheatsheet/
+### Wallace Bruno Gentil :D
---
+++ wb-install.sh
@@ -0,0 +1,165 @@
+#!/bin/bash
+
+# -- Manual Function
+man () {
+    echo " To use this script u need SUDO permission and follows flag:
+
+    wb-install.sh \$1 \$2
+
+    \$1 :
+    man     -> To open manual 
+    install -> To install all customization
+    backup  -> To make backup all confgs 
+
+    \$2 :
+    all     -> All defaults customizations
+    select  -> To select customizations you want
+    
+    "
+}
+
+# -- Check if last output code ($?) is 0 [ OK ] or > [ Fail ]  
+check () {
+    if [ $1 -eq 0 ]; then echo -e "[  Ok  ]" ; else echo -e "[ Fail ]" ; exit 1 ; fi
+}
+
+# -- Update and setup mirror function
+setup () {
+    printf "%-80s" "Set fast mirror and update system"
+    sudo pacman-mirrors --geoip &>/dev/null && sudo pacman -Syyu --noconfirm &>/dev/null
+    check $?
+}
+
+# -- Install Function
+install () {
+    declare -a APPS=(
+        alacritty
+        arandr
+        i3-gaps
+        brave
+        firefox
+        ttf-fira-code
+        blueman
+        arc-gtk-theme
+        cmatrix
+        colorpicker
+        cowsay
+        feh
+        git
+        zsh
+        zsh-completions-git
+    )
+
+    for APP in ${APPS[@]}; do
+        if [[ $(pacman -Qs $APP | grep Nome | wc -l) -eq 0 ]]; then
+            printf "%-80s" "Install - $APP"
+            pacman -S $APP --noconfirm &>/dev/null
+            check $?
+        fi
+    done
+
+    declare -a YAY_PACKAGES=(
+        nerd-fonts-complete
+        visual-studio-code-bin
+        alacritty-themes
+    )
+
+    for YAY_PACKAGE in ${YAY_PACKAGES[@]}; do
+        yay -S $YAY_PACKAGE --noconfirm &>/dev/null
+    done
+
+}
+
+# -- Create folder tree function
+folder () {
+    echo " ~ * Create folder Stage :"
+
+    printf "%-80s" "Create folder tree in home"
+    mkdir -p ~/{cloud/{aws,azure},iac/{ansible,terraform},jenkins,kubernetes/{scripts,examples,helm},monitoring/{elk,zabbix,prometheus},scripts,hashcorp,python,docker} &>/dev/null
+    check $?
+}
+
+# -- Configurate applications function
+config () {
+    echo " ~ * Config Stage :"
+
+    printf "%-80s" "Create - aliasrc    config"
+      cp ./config/aliasrc ~/.config/aliasrc &>/dev/null
+      check $?
+
+    printf "%-80s" "Create - i3         config"
+      yes | cp ./config/i3 ~/.i3/config &>/dev/null
+      check $?
+
+    printf "%-80s" "Create - picom      config" 
+      yes | cp ./config/picom ~/.config/picom.conf &>/dev/null
+      check $?
+
+#    printf "%-80s" "Create - rofi       config"
+
+#    printf "%-80s" "Create - Xresource  config"
+
+#    printf "%-80s" "Create - zsh        config"
+
+    printf "%-80s" "Move wallpapers"
+      cp ./wallpapers ~ &>/dev/null
+
+}
+
+plugins () {
+    echo " ~ * Plugin installing Stage :"
+    echo "Not have plugins to install"
+}
+
+clear_f () {
+    echo " ~ * Clearing Stage :"
+
+    declare -a REMOVES=(
+        conky
+        gufw
+        hexchat
+        hplip
+        avahi
+        gimp
+        vlc
+    )
+
+    for REMOVE in ${REMOVES[@]}; do
+        printf "%-80s" "Unistall - $REMOVE"
+        pacman -Rsc $REMOVE --noconfirm &>/dev/null
+        check $?
+    done
+
+}
+
+# -- Init default script
+wb-init () {
+  # - Step 0 - Update and Set mirror
+  setup
+
+  # - Step 1 - Install all dependencies
+  install 
+
+  # - Step 2 - Create all folder to setup all applications and configurate S.O.
+  folder
+
+  # - Step 3 - Configurate all application in ~/.config
+  config
+
+  # - Step 4 - Install plugins in zsh, vim, kubectl, aws-cli, azure-cli
+  plugins
+
+  # - Step 5 - Clear local temp files and folders
+  clear_f 
+}
+
+clear
+
+echo "* ~ Dotfile Initializing
+      = = = = = = = = = = = = =
+      By chwiee"
+
+wb-init
+
+printf "%-80s" "Install picom-jonaburg-git"
+  yay -S picom-jonaburg-git
