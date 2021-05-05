3d15dd08768e91af87a4ddc6818432ef73185264
chwiee <w.brunoge@gmail.com>
2021-05-04 21:25:43 -0300

    Add Opacity only alacritty

--- wb-install.sh
+++ wb-install.sh
@@ -149,13 +149,13 @@ clear_f () {
 # -- Init default script
 wb-init () {
   # - Step 0 - Update and Set mirror
-  setup
+#  setup
 
   # - Step 1 - Install all dependencies
-  install 
+#  install 
 
   # - Step 2 - Create all folder to setup all applications and configurate S.O.
-  folder
+#  folder
 
   # - Step 3 - Configurate all application in ~/.config
   config
